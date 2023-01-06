129b2324926943b32be34b75f7837af26dc062a5
w-tess <106555819+w-tess@users.noreply.github.com>
2023-01-06 01:22:16 +0100

    Update bfly_test & fft_test

--- butterfly/top/src/bfly_test.m
+++ butterfly/top/src/bfly_test.m
@@ -1,27 +1,28 @@
-% generazione di 10000 vettori di test, ciascuno 
-% costituito da 6 valori rappresentati da interi
-% su 16 bit, nel range +/- (2^15-1)
-randvect = randi([-(2^15-1), 2^15-1], 10000, 6, "int16");
-% definizione di vettori di test personali
+%% generazione di 10000 vettori di test, ciascuno 
+%  costituito da 6 valori rappresentati da interi
+%  su 16 bit, nel range +/- (2^15-1)
+MAX=2^15-1;
+randvect = randi([-MAX, MAX], 10000, 6, "int16");
+
+%% definizione di vettori di test personali
 custvect = zeros(8,6);
-custvect(1,:) = 2^15-1;
-custvect(2,:) = -(2^15-1);
+custvect(1,:) = MAX;
+custvect(2,:) = -MAX;
 custvect(3,:) = 0;
-custvect(4,:) = 2^15-1; custvect(4,6) = -(2^15-1);
-custvect(5,:) = 2^15-1; custvect(5,4) = -(2^15-1);
-custvect(6,:) = 2^15-1; custvect(6,3) = -(2^15-1);
-custvect(7,:) = 2^15-1; custvect(7,3:4) = -(2^15-1);
-custvect(8,:) = 2^15-1; custvect(8,2) = -(2^15-1);
-% unione delle due matrici
+custvect(4,:) = MAX; custvect(4,6) = -MAX;
+custvect(5,:) = MAX; custvect(5,4) = -MAX;
+custvect(6,:) = MAX; custvect(6,3) = -MAX;
+custvect(7,:) = MAX; custvect(7,3:4) = -MAX;
+custvect(8,:) = MAX; custvect(8,2) = -MAX;
+
+%% unione delle due matrici
 allvect = [randvect;custvect];
 nrows = size(allvect,1);
-% scrittura dei vettori di test su file
+
+%% scrittura dei vettori di test su file
 f = fopen("bfly_vectors.txt", "w");
 for i = 1:nrows
-    if i ~= nrows
-        fprintf(f,"%6d %6d %6d %6d %6d %6d\n",allvect(i,:));
-    else
-        fprintf(f,"%6d %6d %6d %6d %6d %6d",allvect(i,:));
-    end
+    fprintf(f,"%6d ",allvect(i,:));
+    if i~=nrows fprintf(f, "\n"); end
 end
 fclose(f);
--- butterfly/top/src/fft_test.m
+++ butterfly/top/src/fft_test.m
@@ -1,5 +1,5 @@
-%% definizione dei segnali di test e dei campioni corrispondenti
-% parametri generali
+%% definizione dei segnali di test per la FFT
+%% parametri generali
 freq=5; % frequenza
 k=8; % fattore moltiplicativo per Fs
 Fs=k*freq; % frequenza di campionamento
@@ -7,72 +7,74 @@ N=16; % Numero di campioni nella FFT
 t=(0:(N-1))/Fs; % intervallo temporale
 df=Fs/N; % risoluzione in frequenza
 f=(-N/2:(N/2-1))*df; % intervallo in frequenza
+MAX=2^15-1;
+% Nomi da fornire ai grafici
+t0=["Onda sinusoidale", "Onda quadra", ...
+    "Onda triangolare", "Funzione porta", ...
+    "Funzione Delta di Dirac", ...
+    "Funzione Delta di Dirac 2", ...
+    "Onda sinusoidale complessa", ...
+    "Onda sinusoidale complessa rumorosa"];
+t1=["FFT onda sinusoidale", "FFT onda quadra", ...
+    "FFT onda triangolare", "FFT funzione porta", ...
+    "FFT funzione Delta di Dirac", ...
+    "FFT funzione Delta di Dirac 2", ...
+    "FFT onda sinusoidale complessa", ...
+    "FFT onda sinusoidale complessa rumorosa"];
 
-% definizione funzione seno
-A0=1; % ampiezza dell'onda
+%% definizione funzione seno
 phi0 = 30*pi/180; % fase dell'onda
-x0=A0*sin(2*pi*freq*t+phi0); % funzione seno
+x0=sin(2*pi*freq*t+phi0); % funzione seno
 
-% definizione onda quadra
-A1=0.7;
+%% definizione onda quadra
 phi1=90*pi/180; % fase dell'onda
-x1=A1*square(2*pi*freq*t+phi1);
+x1=square(2*pi*freq*t+phi1);
 
-% definizione onda a dente di sega
-A2=0.5;
-x2=A2*sawtooth(2*pi*freq*t);
+%% definizione onda a dente di sega
+x2=sawtooth(2*pi*freq*t);
 
-% definizione funzione porta
-A3=1;
-x3=zeros(1,N); x3(N/2-N/8:N/2+N/8)=1;
-x3=A3*x3;
+%% definizione funzione porta
+x3=zeros(1,N); x3(1:N*5/16)=1;
 
-% definizione funzione Delta di Dirac
-A4=1;
+%% definizione funzione Delta di Dirac
 x4=zeros(1,N); x4(1)=1;
-x4=A4*x4;
 
-% definizione funzione Delta di Dirac
-A5=1;
+%% definizione funzione Delta di Dirac 2
 x5=zeros(1,N); x5(N/2-N/8)=1; x5(N/2+N/8)=-1;
-x5=A5*x5;
 
-% definizione funzione Delta di Dirac
-A6=1;
-x6=zeros(1,N); x6(N/2-N/8)=1; x6(N/2+N/8)=-1;
-x6=A6*x6;
+%% definizione funzione sinusoide complessa
+x6=exp(1i*2*pi*freq*t);
 
-% definizione funzione sinusoide complessa
-A7=1;
-x7=A7*exp(1i*2*pi*freq*t);
+%% definizione funzione sinusoidale complessa rumorosa
+A7=0.25;
+x7=A7*exp(1i*2*pi*freq*t) + A7*exp(-1i*2*pi*3/2*freq*t);
+x7=x7 + (0.15)*(1+1i)*randn(size(t));
 
-x=[x0; x1; x2; x3; x4; x5; x6; x7]; % Matrice con i vari segnali
-t0=["Onda sinusoidale", ...
-    "Onda quadra", ...
-    "Onda triangolare", ...
-    "Funzione porta", ...
-    "Funzione Delta di Dirac", ...
-    "Funzione Delta di Dirac", ...
-    "Funzione Delta di Dirac", ...
-    "Onda sinusoidale complessa"];
-t1=["FFT onda sinusoidale", ...
-    "FFT onda quadra", ...
-    "FFT onda triangolare", ...
-    "FFT funzione porta", ...
-    "FFT funzione Delta di Dirac", ...
-    "FFT funzione Delta di Dirac", ...
-    "FFT funzione Delta di Dirac", ...
-    "FFT onda sinusoidale complessa"];
+%% Matrice con i vari segnali
+x=[x0; x1; x2; x3; x4; x5; x6; x7];
 
+%% Stampa dei campioni di test su file
+samplesf=fopen("fft_vectors.txt", "w");
+for i=1:size(x,1)
+    fprintf(samplesf, "%6d ", round(real(x(i,:))*MAX));
+    fprintf(samplesf, "\n");
+    fprintf(samplesf, "%6d ", round(imag(x(i,:))*MAX));
+    if i~=size(x,1) fprintf(samplesf, "\n"); end
+end
+fclose(samplesf);
+
+%% Lettura dei campioni generati dalla FFT
+resultsf="fft_results.txt";
+Y=readmatrix(resultsf);
+Y=Y/MAX;
+
+%% Generazione dei grafici
 close all;
 for i=0:1
     if i==0 figure(1); else figure(2); end 
     for j=0:3
         % FFT dei segnali
         X=fft(x(4*i+j+1,:),N); 
-        % Normalizzo gli spettri per ottenere armoniche
-        % con ampiezze congruenti con i segnali nel tempo
-        if i~=1 || j~=0 X=X/N; end
         % traslo i vettori affinché le componenti 
         % DC si trovino al centro dello spettro
         X=fftshift(X);
@@ -87,11 +89,14 @@ for i=0:1
         grid
         % grafico dello spettro in frequenza
         subplot(4,2,2*j+2); 
+        hold on;
         stem(f, abs(X));
+        stem(f, abs(Y(4*i+j+1,:)));
+        hold off;
         title(t1(4*i+j+1));
-        legend("MATLAB", "FFT_1616.vhd");
+        legend("MATLAB", "FFT\_1616");
         grid
     end
     sgtitle("Simulazione FFT (N=" + num2str(N) + ...
-        ", Fs=" + num2str(k) + "*freq)");
+            ", Fs=" + num2str(k) + "*freq)");
 end
